022986dbb453368a8df77e63f7f23d6fe0624ca3
vanshgautam03 <vanshgautam620@gmail.com>
2024-06-16 14:35:40 -0400

    View Updated and calculations setup completed

--- app/src/main/java/ca/georgiancollege/assignment2/Calculation.kt
+++ app/src/main/java/ca/georgiancollege/assignment2/Calculation.kt
@@ -1,5 +1,6 @@
 package ca.georgiancollege.assignment2
 
+import android.util.Log
 import android.widget.TextView
 import ca.georgiancollege.assignment2.databinding.ActivityMainBinding
 
@@ -51,24 +52,24 @@ class Calculation (databinding: ActivityMainBinding){
     {
         val calculationsTextView: TextView = binding.CalculationsView
         val resultTextView: TextView = binding.Result
-        if(calculationsTextView.text == "0"){
+        if(resultTextView.text == "0"){
             when(numbersTag)
             {
-                "0"-> calculationsTextView.text = "0"
-                "."-> calculationsTextView.text = "0."
-                else -> calculationsTextView.text = numbersTag
+                "0"-> resultTextView.text = "0"
+                "."-> resultTextView.text = "0."
+                else -> resultTextView.text = numbersTag
             }
         }
         else{
             when(numbersTag)
             {
                 "."->{
-                    if(!calculationsTextView.text.toString().contains(".")){
-                        calculationsTextView.text = calculationsTextView.text.toString() + numbersTag
+                    if(!resultTextView.text.toString().contains(".")){
+                        resultTextView.text = resultTextView.text.toString() + numbersTag
                     }
                 }
                 else -> {
-                    calculationsTextView.text = calculationsTextView.text.toString() + numbersTag
+                    resultTextView.text = resultTextView.text.toString() + numbersTag
                 }
             }
         }
@@ -86,45 +87,112 @@ class Calculation (databinding: ActivityMainBinding){
                 resultTextView.text = "0"
             }
             "Backspace" -> {
-                if(calculationsTextView.text.length > 1 ){
-                    if (calculationsTextView.text.toString().length <= 2 && (calculationsTextView.text[0]=='-' ||calculationsTextView.text[0]=='.')){
-                        calculationsTextView.text = calculationsTextView.text.toString().dropLast(2)
-                        calculationsTextView.text = "0"
+                if(resultTextView.text.length > 1 ){
+                    if (resultTextView.text.toString().length <= 2 && (resultTextView.text[0]=='-' ||resultTextView.text[0]=='.')){
+                        resultTextView.text = resultTextView.text.toString().dropLast(2)
+                        resultTextView.text = "0"
                     }else{
-                        calculationsTextView.text = calculationsTextView.text.toString().dropLast(1)
+                        resultTextView.text = resultTextView.text.toString().dropLast(1)
                     }
-                }else if (calculationsTextView.text.length == 1){
-                    calculationsTextView.text = "0"
+                }else if (resultTextView.text.length == 1){
+                    resultTextView.text = "0"
                 }
 
             }
             "PlusMinus" ->{
-                if(calculationsTextView.text.contains(".")){
-                    calculationsTextView.text = (calculationsTextView.text.toString().toDouble() * -1).toString()
+                if(resultTextView.text.contains(".")){
+                    resultTextView.text = (resultTextView.text.toString().toDouble() * -1).toString()
                 }else{
-                    calculationsTextView.text = (calculationsTextView.text.toString().toInt() * -1).toString()
+                    resultTextView.text = (resultTextView.text.toString().toInt() * -1).toString()
                 }
 
             }
             "Plus" ->{
-                calculationsTextView.text = calculationsTextView.text.toString() + "+"
+                if(calculationsTextView.text.toString() == "0"){
+                    calculationsTextView.text = resultTextView.text.toString() + "+"
+                    resultTextView.text = "0"
+                }else{
+                    calculationsTextView.text = calculationsTextView.text.toString() + resultTextView.text.toString() + "+"
+                    resultTextView.text = "0"
+                }
+//                calculationsTextView.text = calculationsTextView.text.toString() + "+"
 
             }
             "Minus" ->{
-                calculationsTextView.text = calculationsTextView.text.toString() + "-"
+                if (calculationsTextView.text.toString() == "0"){
+                    calculationsTextView.text = resultTextView.text.toString() + "-"
+                    resultTextView.text = "0"
+                }else{
+                    calculationsTextView.text = calculationsTextView.text.toString() + resultTextView.text.toString() + "-"
+                    resultTextView.text = "0"
+                }
             }
             "Multiply" ->{
-                calculationsTextView.text = calculationsTextView.text.toString() + "X"
+                if (calculationsTextView.text.toString() == "0"){
+                    calculationsTextView.text = resultTextView.text.toString() + "X"
+                    resultTextView.text = "0"
+                }else{
+                    calculationsTextView.text = calculationsTextView.text.toString() + resultTextView.text.toString() + "X"
+                    resultTextView.text = "0"
+                }
             }
             "Divide" ->{
-                calculationsTextView.text = calculationsTextView.text.toString() + (binding.divide?.text ?:"/")
+                if (calculationsTextView.text.toString() == "0"){
+                    calculationsTextView.text = resultTextView.text.toString() + (binding.divide?.text ?:"/")
+                    resultTextView.text = "0"
+                }else{
+                    calculationsTextView.text = calculationsTextView.text.toString() + resultTextView.text.toString() + (binding.divide?.text ?:"/")
+                    resultTextView.text = "0"
+                }
             }
             "Percent" ->{
-                calculationsTextView.text = calculationsTextView.text.toString() + "%"
+                if (calculationsTextView.text.toString() == "0"){
+                    calculationsTextView.text = resultTextView.text.toString() + "%"
+                    resultTextView.text = "0"
+                }else{
+                    calculationsTextView.text = calculationsTextView.text.toString() + resultTextView.text.toString() + "%"
+                    resultTextView.text = "0"
+                }
             }
             "Equals" -> {
+                val operators = arrayOf('+', '-', '*', '/', '%')
+                if (calculationsTextView.text.toString() == "0"){
+                    calculationsTextView.text = resultTextView.text.toString()
+                    resultTextView.text = resultTextView.text.toString()
+                }else if (operators.contains(calculationsTextView.text.last())){
+                    if (resultTextView.text.toString() == "0"){
+                        calculationsTextView.text = calculationsTextView.text.toString().dropLast(1)
+                    }else{
+                        calculationsTextView.text = calculationsTextView.text.toString() + resultTextView.text.toString()
+                    }
+                }
+                val calculations = calculationsTextView.text.toString()
+                val result = calculate(calculations)
+                resultTextView.text = result.toString()
             }
             else -> calculationsTextView.text = calculationsTextView.text.toString()
         }
+    }
+
+    private fun calculate(calculations: String): Any {
+        val operatorsPrecedence = mapOf('+' to 1, '-' to 1, '*' to 2, '/' to 2, '%' to 3)
+        val operators = operatorsPrecedence.keys
+        return 0.0
+    }
+
+    private fun addition(firstNum: Double, secondNum: Double): Double {
+        return firstNum + secondNum
+    }
+    private fun subtraction(firstNum: Double, secondNum: Double): Double {
+        return firstNum - secondNum
+    }
+    private fun multiplication(firstNum: Double, secondNum: Double): Double {
+        return firstNum * secondNum
+    }
+    private fun division(firstNum: Double, secondNum: Double): Double {
+        return firstNum / secondNum
+    }
+    private fun percentage(firstNum: Double, secondNum: Double): Double {
+        return (firstNum * secondNum) / 100
     }
 }
